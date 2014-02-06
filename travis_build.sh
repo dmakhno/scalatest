c8ca3ef8279b39185d23ad1b0a3c53371ca622f3
Dmytro Makhno <makh.dv@gmail.com>
2014-02-06 12:12:16 +0200

    added gentests, fixup4

--- travis_build.sh
+++ travis_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export JVM_OPTS="-server -Xms2048M -Xmx2048M -Xss8M -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:NewRatio=8 -XX:MaxPermSize=1024M -XX:-UseGCOverheadLimit"
+export JVM_OPTS="-server -Xms2048M -Xmx5120M -Xss8M -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:NewRatio=8 -XX:MaxPermSize=1024M -XX:-UseGCOverheadLimit"
 #this echo is required to keep travis alive, because some compilation parts are silent for more than 10 minutes
 while true; do echo "..."; sleep 60; done &
 sbt ++$TRAVIS_SCALA_VERSION compile
-sbt ++$TRAVIS_SCALA_VERSION "project gentests" "test -- -Xmx5120M"
+sbt ++$TRAVIS_SCALA_VERSION "project gentests" "test"
 kill %1
 
 sbt ++$TRAVIS_SCALA_VERSION testQuick
